d30f3726c1028b24c505f09bebff64f5e37d9f21
DawaDokuna <re357164@uaeh.edu.com>
2023-08-22 19:51:59 -0600

    commit 22/08/2023

--- funciones/inicio.js
+++ funciones/inicio.js
@@ -1,13 +1,14 @@
-$(document).ready(function() {
-    $.ajax({
-        url: document.location.origin + ':8000/saludo',  
-        type: 'GET',
-        dataType: 'json',
-        success: function(response) {
-            console.log(response.message);
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    });
-});
+// $(document).ready(function() {
+//     $.ajax({
+//         url: document.location.origin + ':8000/saludo',  
+//         type: 'GET',
+//         dataType: 'json',
+//         success: function(response) {
+//             console.log(response.message);
+//         },
+//         error: function(error) {
+//             console.log(error);
+//         }
+//     });
+// });
+$("#main").load("/dashboard/inicio.html");
