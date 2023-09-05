717fde33b6ac0b7ea4acbf69bcbf759286bd5488
DawaDokuna <re357164@uaeh.edu.com>
2023-09-04 23:19:42 -0600

    commit 04/09/2023

--- funciones/carga_archivo.js
+++ funciones/carga_archivo.js
@@ -32,16 +32,17 @@ function cargar_tabla() {
   var lines = contents.split("\n");
   var colums_removed = false;
   var columnas = [];
-  var options_columnas ='<option value="">Seleccione...</option>';
+  var options_columnas = '<option value="">Seleccione...</option>';
   var filas = [];
   var tabla = '<table class="table" id="file_table">';
   if (archivo[0].split(".").pop() == "arff") {
     var data = false;
-    for (var i = 0; i < lines.length; i++) {
+    for (var i = 0, j = 0; i < lines.length; i++) {
       var values = lines[i].split(" ");
       if (values[0] == "@attribute") {
         columnas.push(values[1]);
-        options_columnas += '<option value="' + values[1] + '">' + values[1] + '</option>';
+        options_columnas +=
+          '<option value="' + j++ + '">' + values[1] + "</option>";
       } else if (values[0] == "@data") {
         data = true;
       } else if (data) {
@@ -81,7 +82,8 @@ function cargar_tabla() {
     tabla += "<thead><tr>";
     for (let i = 0; i < columnas; i++) {
       tabla += "<th>Columna " + (i + 1) + "</th>";
-      options_columnas += '<option value="Columna ' + (i + 1) + '">Columna ' + (i + 1) + '</option>';
+      options_columnas +=
+        '<option value="' + i + '">Columna ' + (i + 1) + "</option>";
     }
     tabla += "</tr></thead>";
     tabla += "<tbody>";
@@ -96,6 +98,7 @@ function cargar_tabla() {
     tabla += "</table>";
   }
   $("#select_delete_col").html(options_columnas);
+  document.getElementById("result").innerHTML = "";
   document.getElementById("result").innerHTML = tabla;
 
   $("#btn_opciones").attr("hidden", false);
@@ -116,4 +119,38 @@ function cargar_tabla() {
       2
     );
   }
+}
+function comillas() {
+  lineas = archivo[1].split("\n");
+  var archivo_nuevo = [];
+  lineas.forEach((element) => {
+    archivo_nuevo.push(element.replaceAll('"', ""));
+  });
+  archivo[1] = archivo_nuevo.join("\n");
+  cargar_tabla();
+}
+function caracter() {
+  lineas = archivo[1].split("\n");
+  caracter_borrar = $("#caracter_a_borrar").val();
+  var archivo_nuevo = [];
+  lineas.forEach((element) => {
+    archivo_nuevo.push(element.replaceAll(caracter_borrar, ""));
+  });
+  archivo[1] = archivo_nuevo.join("\n");
+  cargar_tabla();
+}
+
+function borrar_columna() {
+  lineas = archivo[1].split("\n");
+  var archivo_nuevo = [];
+  columna_borrar = $("#select_delete_col").val();
+  lineas.forEach((element) => {
+    element.split(",").forEach((element2, index) => {
+      if (index != columna_borrar) {
+        archivo_nuevo.push(element2);
+      }
+    });
+  });
+  archivo[1] = archivo_nuevo.join("\n");
+  cargar_tabla();
 }
