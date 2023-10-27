10c0ad2353d0f1f4ee2f496d655b967e8b2effa7
DawaDokuna <re357164@uaeh.edu.com>
2023-10-26 21:52:30 -0600

    commit -m "calinski y silhouette"

---
+++ funciones/calinski.js
@@ -0,0 +1,16 @@
+async function printer() {
+	if ($("#parametros_form")[0].checkValidity()) {
+		const datos = format_data();
+		const peticion = {};
+		peticion.datos = datos[0];
+		const response = await fetch(document.location.origin + ":8000/calinski", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(peticion),
+		});
+		$("#metric").html(JSON.parse(await response.json()).score);
+		$("#centros_card").attr("hidden", false);
+	}
+}
