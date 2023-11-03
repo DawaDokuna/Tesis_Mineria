f32c91928568701887d133d8fa80fbec1779ebe2
DawaDokuna <re357164@uaeh.edu.com>
2023-11-02 18:06:12 -0600

    ignore /

--- funciones/inicio.js
+++ funciones/inicio.js
@@ -2,13 +2,13 @@ var url = new URL(window.location.href);
 var params = new URLSearchParams(url.search);
 var kMeansValue = params.get("algoritmo");
 var liks = {
-    "archivo": "/vistas/como_crear.html",
-    "kmeans": "/vistas/kmeans.html",
+    "archivo": "vistas/como_crear.html",
+    "kmeans": "vistas/kmeans.html",
     "jerarquico": "/vistas/jerarquico.html",
-    "dbscan": "/vistas/dbscan.html",
-    "davies": "/vistas/davies.html",
-    "calinski": "/vistas/calinski.html",
-    "silh": "/vistas/silh.html"
+    "dbscan": "vistas/dbscan.html",
+    "davies": "vistas/davies.html",
+    "calinski": "vistas/calinski.html",
+    "silh": "vistas/silh.html"
 }
 var link = liks[kMeansValue]??"dashboard/inicio.html";
 $("#main").load(link);
